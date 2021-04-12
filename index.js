d6f54e9f8c907e9d1a0a6da5d9a4ffde87f8139c
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-04-12 19:20:29 -0400

    created all files needed

---
+++ index.js
@@ -0,0 +1,18 @@
+// THIS IS WHERE YOUR MAIN LOGIC WILL LIE
+
+// You would first require ALL of the files and node packages needed
+// Engineer, Manager, Intern, 
+
+// REQUIRE that page-template.js
+// We are receiving that anonymous function
+// Giving the name of pageTemplate
+const pageTemplate = require('./src/page-template.js');
+// And now, we can use that pageTemplate as a function, which can ACCEPT a parameter
+pageTemplate(answers_from_inquirer_prompt);
+// INDEX FILES ARE CONSIDERED THE ENTRY POINT TO YOUR APPLICATION
+
+// IF THIS IS YOUR ENTRY POINT, YOU MUST DO YOUR INQUIRER HERE
+
+// THIS IS WHERE YOU DO YOUR FS WRITEFILE STUFF
+
+// THE DIST FOLDER IS WHERE THE OUTPUT HTML FILES WILL LAND
