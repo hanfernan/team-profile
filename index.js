b5532551a9d8a4d740b3c9f620ac289a056d9b43
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-04-12 22:24:31 -0400

    added prompt questions for all classes

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const inquirer = require("inquirer");
 
 //require files
 const pageTemplate = require('./src/page-template.js');
-const engineer = require("./Engineer");
-const intern = require("./Intern");
-const manager = require("./Manager");
+const employee = require("./lib/Employee");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const manager = require("./lib/Manager");
 
 // JY We are receiving that anonymous function from page-template.js
 // JY Giving the name of pageTemplate
@@ -17,40 +18,56 @@ const manager = require("./Manager");
 // JY And now, we can use that pageTemplate as a function, which can ACCEPT a parameter
 pageTemplate(answers_from_inquirer_prompt);
 
-//Inquirer prompt
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the employee name?',
-            name: 'name',
-        },
-        {
-            type: 'list',
-            message: 'What is the employee position?',
-            name: 'id',
-            choices: ['Engineer', 'Manager', 'Intern']
-        }
-        {
-            type: 'input',
-            message: 'What is the employee email address?',
-            name: 'email',
-        }
-    ])
-    .then()
-// JY THIS IS WHERE YOU DO YOUR FS WRITEFILE STUFF
+//write Inquirer prompt
+const questions = () => {
+    //NOT SURE IF THESE ARE SUPPOSED TO GO HERE?
+    // return inquire.prompt([
+    //     {
+    //         type: 'input',
+    //         message: 'Please enter your team manager name:',
+    //         name: 'name',
+    //     },
+    //     {
+    //         type: 'input',
+    //         message: 'Please enter your team manager ID:',
+    //         name: 'id',
+    //     },
+    //     {
+    //         type: 'input',
+    //         message: 'Please enter your team manager email address:',
+    //         name: 'email',
+    //     },
+    //     {
+    //         type: 'input',
+    //         message: 'Please enter your team manager office number:',
+    //         name: 'office',
+    //     },
+    // ])
+}
+
+
+//write to file  
+function writeToFile(fileName, data) {
+    fs.writeFile(filename, data, err => {
+        if (err) console.log(err)
+    })
+}
+
+  
 
 // JY THE DIST FOLDER IS WHERE THE OUTPUT HTML FILES WILL LAND
 
 
 function runApp() {
  // JY ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
-  function buildTeam() {
+    questions()
+    .then(function buildTeam() {
     // JY Create the output directory if the output path doesn't exist
     if (!fs.existsSync(OUTPUT_DIR)) {
       fs.mkdirSync(OUTPUT_DIR)
     }
     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
-  }
+  })
+  .then(writeToFile())
 }
 runApp();
